a1dda0ca4537244774a14e7c8f01b1c417712b1a
Rybakov-Ilay <ilay106@mail.ru>
2022-10-14 21:03:56 +0300

    fix: check like comeback

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -11,11 +11,11 @@ import Login from "./Login";
 import Register from "./Register";
 import ProtectedRoute from "./ProtectedRoute";
 
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import api from "../utils/Api";
 import * as authMesto from "../utils/authMesto";
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import { Route, Switch, useHistory } from "react-router-dom";
+import {CurrentUserContext} from "../contexts/CurrentUserContext";
+import {Route, Switch, useHistory} from "react-router-dom";
 
 function App() {
     const [currentUser, setCurrentUser] = useState({});
@@ -31,7 +31,7 @@ function App() {
     const [isSingUp, setIsSingUp] = useState(false);
     const history = useHistory();
 
-    function handelSubmitSingUp({ password, email }) {
+    function handelSubmitSingUp({password, email}) {
         authMesto
             .register(password, email)
             .then(() => {
@@ -44,7 +44,7 @@ function App() {
             .finally(() => setIsInfoTooltipPopupOpen(true));
     }
 
-    function handelSubmitLogin({ password, email }) {
+    function handelSubmitLogin({password, email}) {
         localStorage.setItem("email", email);
         authMesto
             .authorize(password, email)
@@ -87,9 +87,7 @@ function App() {
         api
             .changeLikeCardStatus(card._id, isLiked)
             .then((newCard) => {
-                setCards((state) =>
-                    state.map((c) => (c._id === card._id ? newCard : c))
-                );
+                setCards((state) => state.map((c) => (c._id === card._id ? newCard : c)))
             })
             .catch((err) => console.log(err));
     }
@@ -249,7 +247,7 @@ function App() {
                             />
                         </Route>
                     </Switch>
-                    <Footer />
+                    <Footer/>
                 </div>
                 <EditProfilePopup
                     isOpen={isEditProfilePopupOpen}
@@ -280,7 +278,7 @@ function App() {
                     onClose={closeAllPopups}
                     isSingUp={isSingUp}
                 />
-                <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+                <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
             </CurrentUserContext.Provider>
         </div>
     );
