1e23cfcd2f22fb1f3427255d305978fad00b85bf
Rybakov-Ilay <ilay106@mail.ru>
2022-10-14 21:15:39 +0300

    fix: check like test

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -83,7 +83,8 @@ function App() {
 
     function handleCardLike(card) {
         const isLiked = card.likes.some((i) => i._id === currentUser._id);
-
+        console.log(`isLiked = ${isLiked}`);
+        console.log(`card = ${card}`);
         api
             .changeLikeCardStatus(card._id, isLiked)
             .then((newCard) => {
