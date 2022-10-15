46373e2f232765c1da4bc4e0fe0cb5efb4c9b314
Rybakov-Ilay <ilay106@mail.ru>
2022-10-15 10:30:02 +0300

    feat: delete console.log in like

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -82,7 +82,6 @@ function App() {
 
     function handleCardLike(card) {
         const isLiked = card.likes.some((i) => i === currentUser._id);
-
         api
             .changeLikeCardStatus(card._id, isLiked)
             .then((newCard) => {
